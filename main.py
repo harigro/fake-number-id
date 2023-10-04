4e99c45d151091879cc633e2fc0bcd9fd807a6cb
harigro <trierbank@gmail.com>
2023-10-04 14:51:10 +0700

    Mengganti nama pustaka

--- main.py
+++
@@ -1,9 +0,0 @@
-from fake_numberphone_id import indosat, telepon, layanan_operator
-
-
-nomor = telepon(indosat.EMPAT_BELAS)
-ll = layanan_operator(indosat.EMPAT_BELAS)
-
-if __name__ == '__main__':
-    print(nomor)
-    print(ll)
