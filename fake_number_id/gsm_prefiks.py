e870f39ad0f1f07c188c2b178fc0f3eb20779210
harigro <trierbank@gmail.com>
2023-10-08 10:26:12 +0700

    Menambahkan fungsionalitas

--- fake_number_id/gsm_prefiks.py
+++ fake_number_id/gsm_prefiks.py
@@ -1,15 +1,16 @@
-#! pustaka/alat/bin/python3
-
 from faker import Faker
 import phonenumbers
 
 
-def telepon(operator: str) -> str:
+def telepon(operator: str, international: bool = False) -> str:
     fake = Faker(locale="id_iD")
     nomor = '{0}{1}'.format(operator.value, fake.msisdn()[5:])
     nomor_seri = phonenumbers.parse(nomor, "ID")
-    return phonenumbers.format_number(nomor_seri, phonenumbers.PhoneNumberFormat.NATIONAL)
+    if international:
+        return phonenumbers.format_number(nomor_seri, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
+    else:
+        return phonenumbers.format_number(nomor_seri, phonenumbers.PhoneNumberFormat.NATIONAL)
 
-def layanan_operator(operator: str) -> str:
-    return str(operator).split('.')[0]
-
+def layanan_operator(operator: str) -> tuple[str]:
+    data = str(operator).split('.')
+    return (data[0], operator.value)
